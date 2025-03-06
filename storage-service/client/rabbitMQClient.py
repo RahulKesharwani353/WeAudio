b078d2ebb51f6586dacc340daef5941336c428df
rahulkesharwani353 <rahulkesharwani353@gmail.com>
2025-03-06 22:24:41 +0530

    Add RabbitMQ integration for message publishing and update video upload filename format

---
+++ storage-service/client/rabbitMQClient.py
@@ -0,0 +1,23 @@
+import pika, json
+
+RABBITMQ_HOST = "localhost"
+RABBITMQ_PORT = 5672
+RABBITMQ_USER = "guest"  # Default username
+RABBITMQ_PASS = "guest"  # Default password
+QUEUE_NAME = "task_queue"
+
+def get_rabbitmq_connection():
+    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
+    return pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST, RABBITMQ_PORT, '/', credentials))
+
+def get_rabbitmq_channel():
+    connection = get_rabbitmq_connection()
+    return connection.channel()
+
+def publish_message(message):
+    channel = get_rabbitmq_channel()
+    channel.queue_declare(queue=QUEUE_NAME, durable=True)
+    channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=json.dumps(message), properties=pika.BasicProperties(delivery_mode=2))
+    print(f" [x] Sent {message}")
+    connection = get_rabbitmq_connection()
+    connection.close()
--- storage-service/utils/upload.py
+++ storage-service/utils/upload.py
@@ -2,12 +2,13 @@ from fastapi import Depends
 from client.mongoClient import get_async_gridfs, get_sync_db
 import io
 from datetime import datetime
+from client.rabbitMQClient import publish_message
 
 async def upload(f):
     fs = await get_async_gridfs()
     try:
         fid = await fs.upload_from_stream(
-            filename=f.name,
+            filename= f"{datetime.now()}.mp4",
             source=io.BytesIO(f),
             metadata={
                 "content_type": "video/mp4",
@@ -23,4 +24,12 @@ async def upload(f):
         "video_fid": str(fid),
         "mp3_fid": None
     }
+
+    #publish message to rmq
+    try:
+        publish_message(message)
+    except Exception as err:
+        print(err)
+        return "internal server error, rmq level", 500
+    
     return message, 200
